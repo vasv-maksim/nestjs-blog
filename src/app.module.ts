525e17db72f338ca3e78e3b5cb21a30a82efd503
maxim vasev <vasvmaksim@gmail.com>
2022-03-12 13:35:32 +0300

    add moduls

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { PostsModule } from './posts/posts.module';
+import { CommentsModule } from './comments/comments.module';
+import { UsersModule } from './users/users.module';
+import { ConfigModule } from './config/config.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, PostsModule, CommentsModule, UsersModule, ConfigModule],
   controllers: [AppController],
   providers: [AppService],
 })
